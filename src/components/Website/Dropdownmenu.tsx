aad435ef66bf43e6c45824c1c34e1d9e49ed4756
unknown <ee20btech11013@iith.ac.in>
2021-12-25 00:45:50 +0530

    Added header

---
+++ src/components/Website/Dropdownmenu.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { MenuProps } from "@mui/material/Menu";
+import { styled, alpha } from "@mui/material/styles";
+import Menu from "@mui/material/Menu";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import * as styles from "../css/index.module.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+function Dropdownmenu({title,dropwdownoptions}) {
+  const StyledMenu = styled((props: MenuProps) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    "& .MuiPaper-root": {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === "light"
+          ? "rgb(55, 65, 81)"
+          : theme.palette.grey[300],
+      boxShadow:
+        "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+      "& .MuiMenu-list": {
+        padding: "4px 0",
+      },
+      "& .MuiMenuItem-root": {
+        "& .MuiSvgIcon-root": {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        "&:active": {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity
+          ),
+        },
+      },
+    },
+  }));
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  // const dropwdownoptions = ["Edit", "Select", "Paste", "Use"];
+  return (
+    <React.Fragment>
+      <Button
+        style={{ backgroundColor: "#fff" }}
+        id="demo-customized-button"
+        aria-controls="demo-customized-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon style={{ color: "#444444" }} />}
+      >
+        <span className={styles.Links}>{title}</span>
+      </Button>
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          "aria-labelledby": "demo-customized-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        {dropwdownoptions.map((item,index) => (
+          <MenuItem onClick={handleClose} disableRipple >
+            <span className={styles.links} key={index}>{item}</span>
+          </MenuItem>
+        ))}
+      </StyledMenu>
+    </React.Fragment>
+  );
+}
+
+export default Dropdownmenu;
--- src/components/Website/Header.tsx
+++ src/components/Website/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -10,115 +10,200 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import { StylesProvider } from "@mui/styles";
-import * as styles from '../css/index.module.css'
-const pages = ["Home", "About", "Events", "Newsletter", "FAQ"];
+import * as styles from "../css/index.module.css";
+import Dropdownmenu from "./Dropdownmenu";
+import ClearIcon from "@mui/icons-material/Clear";
+// import { makeStyles } from "@mui/styles";
+const pages = ["About", "Home"];
 function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
-
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
+  const [scrolled, setscrolled] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > window.innerHeight / 10) {
+        setscrolled(true);
+      } else {
+        setscrolled(false);
+      }
+    });
+  });
   return (
     <StylesProvider injectFirst>
       <AppBar
-        position="static"
-        className={styles.Header} 
-        style={{ backgroundColor: "white"}}
+        position="sticky"
+        className={styles.Header}
+        style={{
+          backgroundColor: "white",
+          boxShadow: scrolled ? "0px 2px 20px rgb(1 41 112 / 10%)" : "none",
+        }}
       >
         <Container maxWidth="xl">
-        <Container maxWidth="xl">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{
-                mr: 10,
-                ml: 6,
-                display: { xs: "none", md: "flex" },
-                color: "#444444",
-              }}
-            >
-              LOGO
-            </Typography>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{
-                flexGrow: 1,
-                display: { xs: "flex", md: "none" },
-                color: "#444444",
-              }}
-            >
-              LOGO
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page) => (
+          <Container maxWidth="xl">
+            <Toolbar>
+              {/* This is for Laptops */}
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{
+                  ml: 6,
+                  display: { xs: "none", md: "flex" },
+                  color: "#444444",
+                }}
+              >
+                LOGO
+              </Typography>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{
+                  flexGrow: 1,
+                  display: { xs: "flex", md: "none" },
+                  color: "#444444",
+                }}
+              >
+                LOGO
+              </Typography>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: { xs: "none", md: "flex" },
+                  flexFlow: "row-reverse",
+                  mr: 10,
+                }}
+              >
                 <Button
                   disableRipple
-                  key={page}
-                  className={styles.Buttontext}
+                  style={{ backgroundColor: "transparent" }}
                   onClick={handleCloseNavMenu}
                   sx={{ my: 2, mr: 2, display: "block" }}
                 >
-                  <span className={styles.Links}>{page}</span>
+                  <span className={styles.Links}>FAQs</span>
                 </Button>
-              ))}
-            </Box>
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: { xs: "flex", md: "none" },
-                flexFlow: "row-reverse",
-              }}
-            >
-              <IconButton
-                disableRipple
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-                sx={{ color: "#444444" }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
+                <Dropdownmenu
+                  title="Events"
+                  dropwdownoptions={[
+                    "Upcoming ",
+                    "Concluded",
+                    "Newsletter",
+                    "Articles",
+                  ]}
+                />
+                <Dropdownmenu
+                  title="People"
+                  dropwdownoptions={[
+                    "Core Team ",
+                    "Department Faculty Representatives",
+                    "UG Mentors Team",
+                    "PG Mentors Team",
+                    "UG Buddies Team",
+                    "PG Buddies Team",
+                    "Previous Teams",
+                  ]}
+                />
+                <Dropdownmenu
+                  title="Programmes"
+                  dropwdownoptions={["Mentorship Programme", "Buddy Programme"]}
+                />
                 {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
+                  <Button
+                    disableRipple
+                    key={page}
+                    style={{ backgroundColor: "transparent" }}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, mr: 2, display: "block" }}
+                  >
+                    <span className={styles.Links}>{page}</span>
+                  </Button>
                 ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
+              </Box>
+              {/* Now onwards it is mobile version design */}
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: { xs: "flex", md: "none" },
+                  flexFlow: "row-reverse",
+                }}
+              >
+                <IconButton
+                  disableRipple
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                  sx={{ color: "#444444", cursor: "pointer" }}
+                >
+                  {anchorElNav ? <ClearIcon /> : <MenuIcon />}
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.reverse().map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <span className={styles.links}>{page}</span>
+                    </MenuItem>
+                  ))}
+                  <Dropdownmenu
+                  title="Programmes"
+                  dropwdownoptions={["Mentorship Programme", "Buddy Programme"]}
+                />
+                <br/>
+                <Dropdownmenu
+                  title="People"
+                  dropwdownoptions={[
+                    "Core Team ",
+                    "Department Faculty Representatives",
+                    "UG Mentors Team",
+                    "PG Mentors Team",
+                    "UG Buddies Team",
+                    "PG Buddies Team",
+                    "Previous Teams",
+                  ]}
+                />
+                <br/>
+                  <Dropdownmenu
+                  title="Events"
+                  dropwdownoptions={[
+                    "Upcoming ",
+                    "Concluded",
+                    "Newsletter",
+                    "Articles",
+                  ]}
+                  />
+                  <br/>
+                <MenuItem onClick={handleCloseNavMenu}>
+                      <span className={styles.links}>FAQs</span>
+                    </MenuItem>
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
         </Container>
       </AppBar>
     </StylesProvider>

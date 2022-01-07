1384f55b54110dac7fb5b1a7b70e9bace9719c39
Aayush Patel <74599395+Aayush2492@users.noreply.github.com>
2022-01-07 17:44:28 +0530

    Added react components necessary for dropdown

---
+++ src/components/TeamMember.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function TeamMember(props) {
+  return <div></div>;
+}
---
+++ src/components/TeamMembers.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TeamMembers(props) {
+  return <div>{props.department}</div>;
+}
